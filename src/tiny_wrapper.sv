b684362ebf05322f4d928af79e4057f1e19ee022
IAmMarcelJung <MarcelJung97@web.de>
2025-08-20 01:24:41 +0200

    feat: start implementing the wrapper

---
+++ src/tiny_wrapper.sv
@@ -0,0 +1,72 @@
+// SPDX-FileCopyrightText: © 2025 Marcel Jung
+// SPDX-License-Identifier: Apache-2.0
+
+// Adapted from the Tiny Tapeout template
+
+`default_nettype none
+// `include "../../submodules/heichips25-ppwm/src/heichips25_ppwm.sv"
+// `include "../../submodules/heichips25_SDR_new/src/heichips25_template.sv"
+
+module tiny_wrapper (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // Sets which project will be active
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+    wire [7:0] ui_in_ppwm;
+    wire [7:0] uo_out_ppwm;
+    wire [7:0] uio_out_ppwm;
+    wire [7:0] uio_oe_ppwm;
+    wire rst_n_ppwm;
+    wire ena_ppwm;
+
+    wire [7:0] uo_out_sdr;
+    wire [7:0] uio_out_sdr;
+    wire [7:0] uio_oe_sdr;
+    wire rst_n_sdr;
+    wire ena_sdr;
+
+    // If ena = 1, the PPWM project will be active and if ena = 0, the SDR will be active
+    assign ena_sdr = ena;
+    assign ena_ppwm = ~ena_sdr; // Only one project is enabled at the same time
+
+
+    assign uo_out = ena ? uo_out_ppwm : uo_out_sdr;
+    assign uio_out = ena ? uio_out_ppwm : uio_out_sdr;
+    assign uio_oe = ena ? uio_oe_ppwm : uio_oe_sdr;
+
+    // Reset the project that is not enabled. Also reset if the external
+    // reset is triggered.
+    assign rst_n_ppwm = ena_ppwm & rst_n;
+    assign rst_n_sdr = ena_sdr & rst_n;
+
+    // Instantiation of the PPWM project
+    heichips25_ppwm heichips25_ppwm_i (
+        .ui_in(ui_in),
+        .uo_out(uo_out_ppwm),
+        .uio_in(uio_in),
+        .uio_out(uio_out_ppwm),
+        .uio_oe(uio_oe_ppwm),
+        .ena(1'b1), // The internal enable signal is always 1
+        .clk(clk),
+        .rst_n(rst_n_ppwm)
+    );
+
+    // Instantiation of the SDR project
+    // heichips25_template heichips25_template_i (
+    //     .ui_in(ui_in),
+    //     .uo_out(uo_out_sdr),
+    //     .uio_in(uio_in),
+    //     .uio_out(uio_out_sdr),
+    //     .uio_oe(uio_oe_sdr),
+    //     .ena(1'b1), // The internal enable signal is always 1
+    //     .clk(clk),
+    //     .rst_n(rst_n_sdr)
+    // );
+
+endmodule
--- tb/testbench.py
+++ tb/testbench.py
@@ -4,93 +4,166 @@
 import os
 import sys
 from pathlib import Path
+import importlib.util
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.runner import get_runner
-from cocotb.triggers import Timer, ClockCycles
+from cocotb.triggers import Timer, ClockCycles, RisingEdge
+
+# ------------------------------
+# Import the PPWM testbench from the submodule
+# ------------------------------
+this_path = Path(__file__).resolve().parent
+ppwm_tb_path = this_path.parent / "submodules/heichips25-ppwm/tb/testbench.py"
+
+spec = importlib.util.spec_from_file_location("ppwm_tb", ppwm_tb_path)
+ppwm_tb = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(ppwm_tb)
 
 
 @cocotb.test()
-async def counter_test(dut):
-    """Testing the counter of the design."""
-    
+async def pwm_test(dut):
+    """Testing the PWM output behavior."""
+
     # Create a clock with a period of 10ns = 100MHz
-    clock = Clock(dut.clk, 10, 'ns')
+    clock = Clock(dut.clk, 10, "ns")
     await cocotb.start(clock.start())
 
-    dut.ena.value    = 1 # always 1
-    dut.ui_in.value  = 0
+    dut.ena.value = 0  # Enable PPWM, disable SDR
+    dut.ui_in.value = 0
     dut.uio_in.value = 0
 
     # Reset the design for 100ns
     dut.rst_n.value = 0
-    await Timer(100, 'ns')
+    await Timer(100, "ns")
     dut.rst_n.value = 1
-    await Timer(100, 'ns')
+    await Timer(100, "ns")
 
-    # Ensure the otuput is 0x00
-    assert dut.uo_out.value == 0, "Output is not 0!"
+    # Initialize memory with program instructions
+    # 16 x 7-bit instructions
+    program = [
+        0b011_0_001,  # 0 set pwm 3
+        0b011_1_001,  # 1 set reg 3
+        0b0_001_110,  # 2 cmp gcntl < reg
+        0b_0010_111,  # 3 branch #5
+        0b_0010_101,  # 4 jump #6
+        0b001_0_010,  # 5 add pwm 1
+        0b_0001_000,  # 6 wait
+        0b_1010_101,  # 7 jump #2
+        0b0000000,  # ctrl nop
+        0b0000000,  # ctrl nop
+        0b0000000,  # ctrl nop
+        0b0000000,  # ctrl nop
+        0b0000000,  # ctrl nop
+        0b0000000,  # ctrl nop
+        0b0000000,  # ctrl nop
+        0b0000000,  # ctrl nop
+    ]
 
-    # Wait for 10 clock cycles
-    await ClockCycles(dut.clk, 10)
-    
-    # Ensure the otuput is still 0x00
-    assert dut.uo_out.value == 0, "Output is not 0!"
-    
-    # Enable the counter
-    dut.ui_in.value = 1
-    
-    # Wait for 10 clock cycles
-    await ClockCycles(dut.clk, 10)
-    
-    # Ensure the otuput is 10-1
-    assert dut.uo_out.value == 10-1, "Output is not 9!"
-    
+    await exec_ppwm_test(dut, program)
+    # # Load program into memory
+    # await load_program_to_memory(dut, program)
+    #
+    # await ClockCycles(dut.clk, 2)
+    #
+    # # Wait for another half period to complete the cycle
+    # await ClockCycles(dut.clk, 2048 * 8)
+
+    dut.ena.value = 1  # Disable PPWM, enable SDR
+    await exec_ppwm_test(dut, program)
+
+    # # Load program into memory
+    # await load_program_to_memory(dut, program)
+    #
+    # await ClockCycles(dut.clk, 2)
+    #
+    # # Wait for another half period to complete the cycle
+    # await ClockCycles(dut.clk, 2048 * 8)
+
+
+async def exec_ppwm_test(dut, program):
+    """Execute the test for the ppwm module."""
+    # Load program into memory
+    await load_program_to_memory(dut, program)
+
+    await ClockCycles(dut.clk, 2)
+
+    # Wait for another half period to complete the cycle
+    await ClockCycles(dut.clk, 2048 * 8)
+
+
+async def load_program_to_memory(dut, program):
+    """Load a program into the memory module via serial interface."""
+    # Send start bit (high)
+    dut.ui_in[0].value = 1
+    await ClockCycles(dut.clk, 1)
+    for _, instruction in enumerate(program):
+        # Send 7 bits of instruction data (LSB first)
+        print(f"Loading instruction: {instruction:07b}")
+        for bit in range(7):
+            dut.ui_in[0].value = (instruction >> bit) & 1
+            await ClockCycles(dut.clk, 1)
+
     # cocotb documentation: https://docs.cocotb.org/en/stable/refcard.html
-    # cocotb reference card: https://docs.cocotb.org/en/stable/refcard.html
+
 
 if __name__ == "__main__":
-
-    sim         = os.getenv("SIM", "icarus")
-    pdk_root    = os.getenv("PDK_ROOT", "~/.ciel")
-    pdk         = os.getenv("PDK", "ihp-sg13g2")
-    scl         = os.getenv("SCL", "sg13g2_stdcell")
-    gl          = os.getenv("GL", False)
+    sim = os.getenv("SIM", "icarus")
+    pdk_root = os.getenv("PDK_ROOT", "~/.ciel")
+    pdk = os.getenv("PDK", "ihp-sg13g2")
+    scl = os.getenv("SCL", "sg13g2_stdcell")
+    gl = os.getenv("GL", False)
 
     testbench_path = Path(__file__).resolve().parent
-    sources = []#[testbench_path / 'testbench.sv']
+    sources = []  # [testbench_path / 'testbench.sv']
     defines = {}
 
-    MACRO_NL = testbench_path / '../macro/nl/heichips25_template.nl.v'
+    MACRO_NL = testbench_path / "../macro/nl/tiny_wrapper.nl.v"
 
     if gl:
         if not MACRO_NL.exists():
-            print(f"The macro netlist {MACRO_NL} does not exist. Did you implement the macro?")
+            print(
+                f"The macro netlist {MACRO_NL} does not exist. Did you implement the macro?"
+            )
             sys.exit(0)
-    
-        sources.append(Path(pdk_root).expanduser() / pdk / "libs.ref" / scl / "verilog" / f"{scl}.v" )
-        sources.append(MACRO_NL)
-        defines = {'FUNCTIONAL': True, 'UNIT_DELAY': '#0'}
-    else:
-        sources.extend(list(testbench_path.glob('../src/*')))
-        defines = {'RTL': True}
 
-    hdl_toplevel = "heichips25_template"
+        sources.append(
+            Path(pdk_root).expanduser()
+            / pdk
+            / "libs.ref"
+            / scl
+            / "verilog"
+            / f"{scl}.v"
+        )
+        sources.append(MACRO_NL)
+        defines = {"FUNCTIONAL": True, "UNIT_DELAY": "#0"}
+    else:
+        sources.extend(list(testbench_path.glob("../src/*")))
+        sources.extend(list(testbench_path.glob("../submodules/heichips25-ppwm/src/*")))
+        sources.extend(
+            list(testbench_path.glob("../submodules/heichips25_SDR_new/src/*"))
+        )
+        defines = {"RTL": True}
+
+    hdl_toplevel = "tiny_wrapper"
 
     runner = get_runner(sim)
     runner.build(
         sources=sources,
         hdl_toplevel=hdl_toplevel,
         defines=defines,
-        timescale=['1ns', '1ps'],
+        timescale=["1ns", "1ps"],
         waves=True,
-        build_args=['--trace', '--trace-fst', '--trace-structs'] if sim == 'verilator' else ['-gno-specify'],
+        build_args=["--trace", "--trace-fst", "--trace-structs"]
+        if sim == "verilator"
+        else ["-gno-specify"],
     )
 
     runner.test(
         hdl_toplevel=hdl_toplevel,
-        test_module='testbench,',
-        timescale=['1ns', '1ps'],
+        test_module="testbench,",
+        timescale=["1ns", "1ps"],
         waves=True,
-        plusargs=['--trace-file', f'{hdl_toplevel}.fst']  if sim == 'verilator' else [],
+        plusargs=["--trace-file", f"{hdl_toplevel}.fst"] if sim == "verilator" else [],
     )
