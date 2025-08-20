3f108e86abb1b72586c50f537a5c944466bcf0d0
IAmMarcelJung <MarcelJung97@web.de>
2025-08-20 15:01:46 +0200

    test: populate the SystemVerilog testbench with a the tiny wrapper and the instantiation of the PPWM module

--- tb/testbench.sv
+++ tb/testbench.sv
@@ -1,30 +1,50 @@
-`timescale 1ns/1ps
-
-module testbench;
-
-    initial begin
-        $dumpfile("waveforms.fst");
-        $dumpvars(0, testbench);
-    end
-
-    logic [7:0] ui_in;
-    logic [7:0] uo_out;
-    logic [7:0] uio_in;
-    logic [7:0] uio_out;
-    logic [7:0] uio_oe;
-    logic       ena;
-    logic       clk;
-    logic       rst_n;
-
-    tt_um_example tt_um_example (
-        .ui_in,
-        .uo_out,
-        .uio_in,
-        .uio_out,
-        .uio_oe,
-        .ena,
-        .clk,
-        .rst_n
+module testbench (
+    input clk,
+    input rst_n,
+    input ena,
+    input [7:0] ui_in,
+    input [7:0] uio_in,
+    output [7:0] uo_out_wrapper,
+    output [7:0] uio_out_wrapper,
+    output [7:0] uio_oe_wrapper,
+    output [7:0] uo_out_ppwm,
+    output [7:0] uio_out_ppwm,
+    output [7:0] uio_oe_ppwm,
+    output [7:0] uo_out_sdr,
+    output [7:0] uio_out_sdr,
+    output [7:0] uio_oe_sdr
+);
+    heichips25_ppwm ppwm_i (
+        .clk(clk),
+        .rst_n(rst_n),
+        .ena(ena),
+        .ui_in(ui_in),
+        .uio_in(uio_in),
+        .uo_out(uo_out_ppwm),
+        .uio_out(uio_out_ppwm),
+        .uio_oe(uio_oe_ppwm)
     );
 
+    tiny_wrapper tiny_wrapper_i (
+        .clk(clk),
+        .rst_n(rst_n),
+        .ena(ena),
+        .ui_in(ui_in),
+        .uio_in(uio_in),
+        .uo_out(uo_out_wrapper),
+        .uio_out(uio_out_wrapper),
+        .uio_oe(uio_oe_wrapper)
+    );
+
+    // FIXME: uncomment once the design is fixed
+    // heichips25_template heichpis_sdr_i (
+    //     .clk(clk),
+    //     .rst_n(rst_n),
+    //     .ena(ena),
+    //     .ui_in(ui_in),
+    //     .uio_in(uio_in),
+    //     .uo_out(uo_out_ppwm),
+    //     .uio_out(uio_out_ppwm),
+    //     .uio_oe(uio_oe_ppwm)
+    // );
 endmodule
