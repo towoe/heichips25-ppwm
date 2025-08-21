8c2ed9b19db989bcf734776f38d1620e13182318
IAmMarcelJung <MarcelJung97@web.de>
2025-08-21 14:15:53 +0200

    doc: improve comment for enable signals

--- src/tiny_wrapper.sv
+++ src/tiny_wrapper.sv
@@ -30,7 +30,8 @@ module tiny_wrapper (
     wire rst_n_sdr;
     wire ena_sdr;
 
-    // If ena = 0, the PPWM project will be active and if ena = 1, the SDR will be active
+
+    // If ena = 0, the PPWM project will be active and if ena = 1, the SDR project will be active
     assign ena_sdr = ena;
     assign ena_ppwm = ~ena_sdr; // Only one project is enabled at the same time
 
