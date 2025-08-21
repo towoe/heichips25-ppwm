b436aaa017f5e805e46025dd88ee7725c02f7297
IAmMarcelJung <MarcelJung97@web.de>
2025-08-21 11:28:32 +0200

    test: update testbench to fit the updates in the submodule
    Create constants for the program instructions
    
    Use the load_program_to_memory() method from the imported testbench instead of defining it locally

--- tb/testbench.py
+++ tb/testbench.py
@@ -23,6 +23,46 @@ spec = importlib.util.spec_from_file_location("ppwm_tb", ppwm_tb_path)
 ppwm_tb = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(ppwm_tb)
 
+# Initialize memory with program instructions
+# 16 x 7-bit instructions
+PROGRAM = [
+    0b011_0_001,  # 0 set pwm 3
+    0b011_1_001,  # 1 set reg 3
+    0b0_001_110,  # 2 cmp gcntl < reg
+    0b_0010_111,  # 3 branch #5
+    0b_0010_101,  # 4 jump #6
+    0b001_0_010,  # 5 add pwm 1
+    0b_0001_000,  # 6 wait
+    0b_1010_101,  # 7 jump #2
+    0b0000000,  # ctrl nop
+    0b0000000,  # ctrl nop
+    0b0000000,  # ctrl nop
+    0b0000000,  # ctrl nop
+    0b0000000,  # ctrl nop
+    0b0000000,  # ctrl nop
+    0b0000000,  # ctrl nop
+    0b0000000,  # ctrl nop
+]
+
+PROGRAM_TEST_MV = [
+    0b011_1_001,  # 0 set reg, 3
+    0b11_1_1_011,  # 1 shift reg left
+    0b00_1_1_011,  # 2 shift reg left
+    0b00_1_1_011,  # 3 shift reg left
+    0b0_001_110,  # 4 cmp global counter < reg
+    0b0011_111,  # 5 branch #8
+    0b0_000_100,  # 6 mv reg to pwm
+    0b0010_101,  # 7 jump +2
+    0b0_100_100,  # 8 mv global counter l to pwm
+    0b0001_000,  # 9 ctrl wait
+    0b1010_101,  # a jump #4
+    0b0000000,  # b
+    0b0000000,  # c
+    0b0000000,  # d
+    0b0000000,  # e
+    0b0000000,  # f
+]
+
 
 @cocotb.test()
 async def compare_wrapper_vs_gold(dut):
@@ -42,44 +82,33 @@ async def compare_wrapper_vs_gold(dut):
     dut.rst_n.value = 1
     await Timer(100, "ns")
 
-    # Initialize memory with program instructions
-    # 16 x 7-bit instructions
-    program = [
-        0b011_0_001,  # 0 set pwm 3
-        0b011_1_001,  # 1 set reg 3
-        0b0_001_110,  # 2 cmp gcntl < reg
-        0b_0010_111,  # 3 branch #5
-        0b_0010_101,  # 4 jump #6
-        0b001_0_010,  # 5 add pwm 1
-        0b_0001_000,  # 6 wait
-        0b_1010_101,  # 7 jump #2
-        0b0000000,  # ctrl nop
-        0b0000000,  # ctrl nop
-        0b0000000,  # ctrl nop
-        0b0000000,  # ctrl nop
-        0b0000000,  # ctrl nop
-        0b0000000,  # ctrl nop
-        0b0000000,  # ctrl nop
-        0b0000000,  # ctrl nop
-    ]
-
     cocotb.start_soon(checker(dut))
 
-    await exec_ppwm_test(dut, program)
+    await exec_ppwm_test(dut, PROGRAM)
 
     dut.ena.value = 1  # Disable PPWM, enable SDR
-    await exec_ppwm_test(dut, program)
+    await exec_ppwm_test(dut, PROGRAM_TEST_MV)
 
 
 async def exec_ppwm_test(dut, program):
     """Execute the test for the ppwm module."""
     # Load program into memory
-    await load_program_to_memory(dut, program)
-
+    await ClockCycles(dut.clk, 1)
+    await ppwm_tb.load_program_to_memory(dut, PROGRAM)
     await ClockCycles(dut.clk, 2)
-
     # Wait for another half period to complete the cycle
-    await ClockCycles(dut.clk, 2048 * 8)
+    await ClockCycles(dut.clk, 2048 * 24)
+
+    # Reset the design for 100ns
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+    # Load program into memory
+    await ppwm_tb.load_program_to_memory(dut, PROGRAM_TEST_MV)
+    await ClockCycles(dut.clk, 2)
+    # Wait for another half period to complete the cycle
+    await ClockCycles(dut.clk, 2048 * 24)
 
 
 async def checker(dut):
@@ -101,21 +130,6 @@ async def checker(dut):
             # assert dut.tiny_wrapper_i.uo_out.value == dut.sdr_i.uo_out.value, (
             #     f"Mismatch: wrapper={dut.tiny_wrapper_i.uo_out.value} sdr={dut.sdr_i.uo_out.value}"
             # )
-
-
-async def load_program_to_memory(dut, program):
-    """Load a program into the memory module via serial interface."""
-    # Send start bit (high)
-    dut.ui_in[0].value = 1
-    await ClockCycles(dut.clk, 1)
-    for _, instruction in enumerate(program):
-        # Send 7 bits of instruction data (LSB first)
-        print(f"Loading instruction: {instruction:07b}")
-        for bit in range(7):
-            dut.ui_in[0].value = (instruction >> bit) & 1
-            await ClockCycles(dut.clk, 1)
-
-    # cocotb documentation: https://docs.cocotb.org/en/stable/refcard.html
 
 
 if __name__ == "__main__":
